59a13f975ae43bf8dd8327989d8da6d678952d44
Ayaz Ahmad <97458144+iam-ayazahmad@users.noreply.github.com>
2022-04-25 15:14:32 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { EmployeeList } from "./components/EmployeeList";
+import { EmployeeDetails } from "./components/EmployeeDetails";
+import { Admin } from "./components/Admin";
+// import { ProtectedRoute } from "./components/PrivateRoute";
+import { Navbar } from "./components/Navbar";
+import { Logout } from "./components/Logout";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
+
+{/* <Routes>Routes here</Routes> */}
